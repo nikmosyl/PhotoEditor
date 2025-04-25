29f1d9a151a67a20bb602de7f8674f347dfac6ed
nikita <nikmosyl@gmail.com>
2025-04-25 12:16:50 +0400

    Initial Commit

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by nikita on 25.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
