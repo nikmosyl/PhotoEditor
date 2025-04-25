d5936e213448cd65ca10a57817b2f3c6d99c6ebe
nikita <nikmosyl@gmail.com>
2025-04-25 17:40:42 +0400

    add TextFieldWithTitle

---
+++ PhotoEditor/Modules/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  PhotoEditor
+//
+//  Created by nikita on 25.04.2025.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ PhotoEditor/Modules/Login/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  PhotoEditor
+//
+//  Created by nikita on 25.04.2025.
+//
+
+import Foundation
